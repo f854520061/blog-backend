5f251c0442845ce6be3391b1d9a78e1a3385ef04
freexzw <7924338+freexzw@user.noreply.gitee.com>
2021-02-09 17:26:47 +0800

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '175399cacfcc9584e92cb5e220dd3f41'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bbc578b0-f988-4621-9cd4-1e271d93c10c'
+};
